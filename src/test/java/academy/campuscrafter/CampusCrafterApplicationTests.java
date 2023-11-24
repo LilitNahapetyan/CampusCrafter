26a234a692daf420abf54499df2e09efb183a532
Lilit <nahapetyanlilit01@gmail.com>
2023-11-24 21:00:14 +0400

    Init commit

---
+++ src/test/java/academy/campuscrafter/CampusCrafterApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.campuscrafter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusCrafterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
